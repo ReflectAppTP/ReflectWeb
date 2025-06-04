572e4bbaccb27cd9de6b04e0f74165a7a0b52e6c
roman <r3wqaxz@yandex.ru>
2025-06-04 22:52:23 +0300

    Allowed hosts update

--- ReflectWeb/settings_prod.py
+++ ReflectWeb/settings_prod.py
@@ -2,7 +2,7 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['reflect-app.ru', 'www.reflect-app.ru', '185.185.71.233']
+ALLOWED_HOSTS = ['reflect-app.ru', 'www.reflect-app.ru', '185.185.71.233', 'web']
 
 # Настройки базы данных PostgreSQL
 DATABASES = {
